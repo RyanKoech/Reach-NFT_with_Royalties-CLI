d7422afe8263533def2e95b47050b79aa68ac313
Ryan Koech <sirryankoech@gmail.com>
2022-04-29 06:56:59 +0300

    Created Basic Console Program to Implement the Reach Code

--- index.mjs
+++ index.mjs
@@ -4,24 +4,42 @@ const stdlib = loadStdlib(process.env);
 
 const startingBalance = stdlib.parseCurrency(100);
 
-const [ accAlice, accBob ] =
+const fmt = (x) => stdlib.formatCurrency(x, 4);
+
+const [ creatorAccount, buyerAccount ] =
   await stdlib.newTestAccounts(2, startingBalance);
-console.log('Hello, Alice and Bob!');
+console.log('Try out my first Reach Application');
+
+const creatorAccAddress = creatorAccount.getAddress();
 
 console.log('Launching...');
-const ctcAlice = accAlice.contract(backend);
-const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
+const creatorContract = creatorAccount.contract(backend);
+const buyerContract = buyerAccount.contract(backend, creatorContract.getInfo());
 
 console.log('Starting backends...');
 await Promise.all([
-  backend.Alice(ctcAlice, {
+  backend.Creator(creatorContract, {
     ...stdlib.hasRandom,
+    createNFT: () => {
+      return {
+        owner: creatorAccAddress,
+        creator: creatorAccAddress,
+        // name : "",
+        // uri: "",
+        price: stdlib.parseCurrency(1.1),
+        royalty: 0
+      }
+    }
     // implement Alice's interact object here
   }),
-  backend.Bob(ctcBob, {
+  backend.Buyer(buyerContract, {
     ...stdlib.hasRandom,
+    getNFTInfo: (nftInfo) => {
+      console.log("NFT...")
+      console.log(nftInfo)
+    }
     // implement Bob's interact object here
   }),
 ]);
 
-console.log('Goodbye, Alice and Bob!');
+console.log('Goodbye, Crrator and Buyer!');
