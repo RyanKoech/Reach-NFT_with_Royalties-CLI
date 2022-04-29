522d8950bbcce5803dcce0e48807121d85fcb493
Ryan Koech <sirryankoech@gmail.com>
2022-04-29 10:12:00 +0300

    Added More Code for Terminal Interface

--- index.mjs
+++ index.mjs
@@ -5,6 +5,7 @@ const stdlib = loadStdlib(process.env);
 const startingBalance = stdlib.parseCurrency(100);
 
 const fmt = (x) => stdlib.formatCurrency(x, 4);
+const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
 
 const [ creatorAccount, buyerAccount ] =
   await stdlib.newTestAccounts(2, startingBalance);
@@ -16,30 +17,45 @@ console.log('Launching...');
 const creatorContract = creatorAccount.contract(backend);
 const buyerContract = buyerAccount.contract(backend, creatorContract.getInfo());
 
+const BaseUser = {
+  ...stdlib.hasRandom,
+  getSenderAddress : () => {}
+}
+
 console.log('Starting backends...');
 await Promise.all([
   backend.Creator(creatorContract, {
     ...stdlib.hasRandom,
+    getSenderAddress: creatorAccount.getAddress(),
+    deadline: { ETH: 100, ALGO: 5, CFX: 1000 }[stdlib.connector],
     createNFT: () => {
       return {
         owner: creatorAccAddress,
         creator: creatorAccAddress,
         // name : "",
         // uri: "",
-        price: stdlib.parseCurrency(1.1),
-        royalty: 0
+        price: stdlib.parseCurrency(20.5),
+        royalty: 25
       }
     }
     // implement Alice's interact object here
   }),
   backend.Buyer(buyerContract, {
     ...stdlib.hasRandom,
+    getSenderAddress: buyerAccount.getAddress(),
     getNFTInfo: (nftInfo) => {
       console.log("NFT...")
       console.log(nftInfo)
+    },
+    buyNFT: (price) => {
+      console.log("Buyer accepts to buy NFT at ALGO: " + fmt(price) );
     }
     // implement Bob's interact object here
   }),
 ]);
 
-console.log('Goodbye, Crrator and Buyer!');
+const buyerAfter = await getBalance(buyerAccount);
+const creatorAfter = await getBalance(creatorAccount);
+console.log("Buyer now left with: " + buyerAfter);
+console.log("Creator now left with: " + creatorAfter);
+console.log('Goodbye, Creator and Buyer!');
