f27eb3329f95accff21d773c876b004f0e2b9ab1
Ryan Koech <sirryankoech@gmail.com>
2022-05-01 15:35:47 +0300

    Completed CLI code

--- index.mjs
+++ index.mjs
@@ -1,61 +1,104 @@
-import {loadStdlib} from '@reach-sh/stdlib';
+import { loadStdlib } from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
-const stdlib = loadStdlib(process.env);
 
-const startingBalance = stdlib.parseCurrency(100);
+(async () => {
+    const stdlib = await loadStdlib();
+    const startingBalance = stdlib.parseCurrency(10);
+ 
 
-const fmt = (x) => stdlib.formatCurrency(x, 4);
-const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
+    const accCreator = await stdlib.newTestAccount(startingBalance);
+    const accBidderA = await stdlib.newTestAccount(startingBalance);
+    const accBidderB = await stdlib.newTestAccount(startingBalance);
+    const addessBidderA = accBidderA.getAddress();
 
-const [ creatorAccount, buyerAccount ] =
-  await stdlib.newTestAccounts(2, startingBalance);
-console.log('Try out my first Reach Application');
 
-const creatorAccAddress = creatorAccount.getAddress();
 
-console.log('Launching...');
-const creatorContract = creatorAccount.contract(backend);
-const buyerContract = buyerAccount.contract(backend, creatorContract.getInfo());
+    const fmt = (x) => stdlib.formatCurrency(x, 4);
+    const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
+ 
+    const ctcCreator = accCreator.contract(backend);
+    const ctcInfo = ctcCreator.getInfo(); 
 
-const BaseUser = {
-  ...stdlib.hasRandom,
-  getSenderAddress : () => {}
-}
+    const bids = {
+        "BiddderA": {
+            maxBid: stdlib.parseCurrency(9),
+        },
+        "BidderB": {
+            maxBid: stdlib.parseCurrency(6),
+        },
+    };
 
-console.log('Starting backends...');
-await Promise.all([
-  backend.Creator(creatorContract, {
-    ...stdlib.hasRandom,
-    getSenderAddress: creatorAccount.getAddress(),
-    deadline: { ETH: 100, ALGO: 5, CFX: 1000 }[stdlib.connector],
-    createNFT: () => {
-      return {
-        owner: creatorAccAddress,
-        creator: creatorAccAddress,
-        // name : "",
-        // uri: "",
-        price: stdlib.parseCurrency(20.5),
-        royalty: 25
-      }
+    const id = stdlib.randomUInt();
+    const deadline = 200;
+    const meter = {
+        meter: 0,
+        bidder: "",
     }
-    // implement Alice's interact object here
-  }),
-  backend.Buyer(buyerContract, {
-    ...stdlib.hasRandom,
-    getSenderAddress: buyerAccount.getAddress(),
-    getNFTInfo: (nftInfo) => {
-      console.log("NFT...")
-      console.log(nftInfo)
-    },
-    buyNFT: (price) => {
-      console.log("Buyer accepts to buy NFT at ALGO: " + fmt(price) );
-    }
-    // implement Bob's interact object here
-  }),
-]);
+    
+    const Common = (who) => ({
+        seeOutcome: (price, address) => {
+            const winner = (stdlib.addressEq(addessBidderA, address)) ? "BiddderA" : "BidderB";
+            console.log(`${who} Saw The Winner is: `, winner);
+        },
+        showBid: (bid) => {
+            console.log(`${who} saw the bid of ${fmt(bid)} from ${meter.bidder}` );
+        },
+        informTimeout: () => {
+            console.log("Payment was not performed.")
+            process.exit(0);
+        },
+        isAuctionOn: () => {
+            meter.meter += 1;
+            if(meter.meter == 3){
+                console.log(`${who} ended auction.`)
+                return false;
+            };
+            console.log(`${who} started the auction.`)
+            return true;
+        }
+    });
 
-const buyerAfter = await getBalance(buyerAccount);
-const creatorAfter = await getBalance(creatorAccount);
-console.log("Buyer now left with: " + buyerAfter);
-console.log("Creator now left with: " + creatorAfter);
-console.log('Goodbye, Creator and Buyer!');
+    const makeOwner = (acc, who) => {
+        const ctc = acc.contract(backend, ctcInfo);
+        return backend.Bidder(ctc, {
+            ...Common(who),
+            getBid: (price) => {
+              console.log(who," see's the price as: ", fmt(price))
+                if (stdlib.add(price, stdlib.parseCurrency(1)) < bids[who].maxBid) {
+                    const bid = stdlib.add(price, stdlib.parseCurrency(1));
+                    console.log(who," therefore bids ", fmt(bid), "ALGO")
+                    meter.bidder = who
+                    return bid;
+                };
+                return price;
+            }
+        })
+    };
+
+    await Promise.all([
+        backend.Creator(
+            ctcCreator,{
+            ...Common("Creator"),
+            createNFT: () => {
+              return {
+                basePrice: stdlib.parseCurrency(3), 
+                royalty: 49, 
+                uri: ""
+              }
+            },
+            getId: id,
+            deadline: deadline,
+        }),
+
+        makeOwner(accBidderA, "BiddderA"),
+        makeOwner(accBidderB, "BidderB"),
+    ]);
+
+
+  const bobAfter = await getBalance(accBidderB);
+  const aliceAfter = await getBalance(accBidderA);
+  const creatorAfter = await getBalance(accCreator);
+  console.log("BidderB now left with: " + bobAfter);
+  console.log("BiddderA now left with: " + aliceAfter);
+  console.log("Creator now left with: " + creatorAfter);
+})();
